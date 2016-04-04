ec4e4efb84892043eb13e821fa8dd3b81b84c67b
RetroPoktan <575508330@qq.com>
2016-04-04 22:23:54 +0800

    add ptr

---
+++ src/main/java/com/retropoktan/rptrello/ui/base/SwipeRefreshBaseActivity.java
@@ -0,0 +1,43 @@
+package com.retropoktan.rptrello.ui.base;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+
+import com.retropoktan.rptrello.R;
+
+import butterknife.Bind;
+
+/**
+ * Created by RetroPoktan on 4/4/16.
+ */
+public abstract class SwipeRefreshBaseActivity extends ToolbarBaseActivity {
+
+    protected boolean isRefreshing;
+    @Bind(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout swipeRefreshLayout;
+    @Bind(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    public boolean isRefreshing() {
+        return isRefreshing;
+    }
+
+    @Override
+    protected int setContentView() {
+        return R.layout.layout_ptr_recycler;
+    }
+
+    @Override
+    protected void initViews() {
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                onSwipeRefresh();
+            }
+        });
+    }
+
+    protected abstract void onSwipeRefresh();
+
+}
